07e7dce0c6e6aedf4c729b7ee5ad3f3b256482b2
kenyafan <kenyafan321@gmail.com>
2024-03-27 20:02:08 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,67 @@ import "modern-normalize";
 import "./index.css";
 import Feedback from "./components/Feedback/Feedback";
 import Options from "./components/Options/Options";
+import Description from "./components/Description/Description";
+import React, { useState, useEffect } from "react";
+import Notification from "./components/Notification/Notification";
 
 const App = () => {
+  const options = ["good", "neutral", "bad"];
+
   const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
   });
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const positiveFeedbackPercentage = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
+  const handleReset = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const updateFeedback = (value) => {
+    setFeedback((prev) => ({ ...prev, [value]: prev[value] + 1 }));
+  };
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  useEffect(() => {
+    const storedFeedback = localStorage.getItem("feedback");
+    if (storedFeedback) {
+      setFeedback(JSON.parse(storedFeedback));
+    } else {
+      setFeedback({ good: 0, neutral: 0, bad: 0 });
+    }
+  }, []);
+
   return (
     <>
-      <div className="titleWrapper">
-        <h1>Sip Happens Café</h1>
-        <p>
-          Please leave your feedback about our service by selecting one of the
-          options below.
-        </p>
-      </div>
-      <Feedback />
-      <Options />
+      <Description />
+      <Options
+        options={options}
+        feedback={feedback}
+        updateFeedback={updateFeedback}
+        onReset={handleReset}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={totalFeedback}
+          positivePercentage={positiveFeedbackPercentage}
+        />
+      ) : (
+        <Notification message="No feedback yet" />
+      )}
     </>
   );
 };
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,6 +1,24 @@
-import c from "./Options.module.css";
-const Options = () => {
-  return <div></div>;
+import s from "./Options.module.css";
+
+const Options = ({ options, updateFeedback, onReset, totalFeedback }) => {
+  return (
+    <ul className={s.options}>
+      {options.map((option) => (
+        <li key={option} className={s.option}>
+          <button className={s.button} onClick={() => updateFeedback(option)}>
+            {option}
+          </button>
+        </li>
+      ))}
+      {totalFeedback > 0 && (
+        <li>
+          <button className={s.button} type="button" onClick={onReset}>
+            Reset
+          </button>
+        </li>
+      )}
+    </ul>
+  );
 };
 
 export default Options;

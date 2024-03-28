fb773ec32555256f62d148601ee08590b2b4c371
kenyafan <kenyafan321@gmail.com>
2024-03-28 16:01:32 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,27 @@ import "./index.css";
 import Feedback from "./components/Feedback/Feedback";
 import Options from "./components/Options/Options";
 import Description from "./components/Description/Description";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Notification from "./components/Notification/Notification";
 
 const App = () => {
   const options = ["good", "neutral", "bad"];
 
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [feedback, setFeedback] = useState(() => {
+    const savedData = JSON.parse(window.localStorage.getItem("feedbackData"));
+    if (savedData) {
+      return savedData;
+    }
+    return { good: 0, neutral: 0, bad: 0 };
   });
+
+  useEffect(() => {
+    window.localStorage.setItem("feedbackData", JSON.stringify(feedback));
+  }, [feedback]);
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
-  const positiveFeedbackPercentage = totalFeedback
+  const positivePercentage = totalFeedback
     ? Math.round((feedback.good / totalFeedback) * 100)
     : 0;
 
@@ -28,19 +34,6 @@ const App = () => {
   const updateFeedback = (value) => {
     setFeedback((prev) => ({ ...prev, [value]: prev[value] + 1 }));
   };
-
-  useEffect(() => {
-    localStorage.setItem("feedback", JSON.stringify(feedback));
-  }, [feedback]);
-
-  useEffect(() => {
-    const storedFeedback = localStorage.getItem("feedback");
-    if (storedFeedback) {
-      setFeedback(JSON.parse(storedFeedback));
-    } else {
-      setFeedback({ good: 0, neutral: 0, bad: 0 });
-    }
-  }, []);
 
   return (
     <>
@@ -58,7 +51,7 @@ const App = () => {
           neutral={feedback.neutral}
           bad={feedback.bad}
           total={totalFeedback}
-          positivePercentage={positiveFeedbackPercentage}
+          positivePercentage={positivePercentage}
         />
       ) : (
         <Notification message="No feedback yet" />
